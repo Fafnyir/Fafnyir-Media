23d3b6afd8075953bea79f480c0dea8d21800968
Fafnyir <34715459+Fafnyir@users.noreply.github.com>
2024-03-23 15:32:52 -0400

    Sync

--- Fafnyir.lua
+++ Fafnyir.lua
@@ -13,17 +13,17 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   FONT
 -- -----
-LSM:Register ("font", "- The Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Regular.ttf]])
-LSM:Register ("font", "- The Bold Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Bold.ttf]])
-LSM:Register ("font", "- The Medium Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Medium.ttf]])
-LSM:Register ("font", "- DAMAGE", [[Interface\Addons\Fafnyir-Media\fonts\Browood-Regular.ttf]])
-LSM:Register ("font", "- The Light Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Light.ttf]])
+LSM:Register ("font", "- The Font", [[Interface\Addons\SharedMedia_Fafnyir\fonts\Fafnyir-Regular.ttf]])
+LSM:Register ("font", "- The Bold Font", [[Interface\Addons\SharedMedia_Fafnyir\fonts\Fafnyir-Bold.ttf]])
+LSM:Register ("font", "- The Medium Font", [[Interface\Addons\SharedMedia_Fafnyir\fonts\Fafnyir-Medium.ttf]])
+LSM:Register ("font", "- DAMAGE", [[Interface\Addons\SharedMedia_Fafnyir\fonts\Browood-Regular.ttf]])
+LSM:Register ("font", "- The Light Font", [[Interface\Addons\SharedMedia_Fafnyir\fonts\Fafnyir-Light.ttf]])
 --   SOUND
 -- -----
 
 -- -----
 --   STATUSBAR
 -- -----
-LSM:Register ("statusbar", "- Half", [[Interface\Addons\Fafnyir-Media\statusbar\Half.tga]])
-LSM:Register ("statusbar", "- Gradient", [[Interface\Addons\Fafnyir-Media\statusbar\Gradient.tga]])
-LSM:Register ("statusbar", "- Clean", [[Interface\Addons\Fafnyir-Media\statusbar\Clean.tga]])
+LSM:Register ("statusbar", "- Half", [[Interface\Addons\SharedMedia_Fafnyir\statusbar\Half.tga]])
+LSM:Register ("statusbar", "- Gradient", [[Interface\Addons\SharedMedia_Fafnyir\statusbar\Gradient.tga]])
+LSM:Register ("statusbar", "- Clean", [[Interface\Addons\SharedMedia_Fafnyir\statusbar\Clean.tga]])
