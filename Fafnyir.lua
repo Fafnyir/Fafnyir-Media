7df4a7a0961141ef087aa87a1860c2b0230e451b
Fafnyir <34715459+Fafnyir@users.noreply.github.com>
2024-03-23 06:58:01 -0400

    Initial commit

---
+++ Fafnyir.lua
@@ -0,0 +1,29 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+
+-- -----
+--   FONT
+-- -----
+LSM:Register ("font", "- The Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Regular.ttf]])
+LSM:Register ("font", "- The Bold Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Bold.ttf]])
+LSM:Register ("font", "- The Medium Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Medium.ttf]])
+LSM:Register ("font", "- DAMAGE", [[Interface\Addons\Fafnyir-Media\fonts\Browood-Regular.ttf]])
+LSM:Register ("font", "- The Light Font", [[Interface\Addons\Fafnyir-Media\fonts\Fafnyir-Light.ttf]])
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register ("statusbar", "- Half", [[Interface\Addons\Fafnyir-Media\statusbar\Half.tga]])
+LSM:Register ("statusbar", "- Gradient", [[Interface\Addons\Fafnyir-Media\statusbar\Gradient.tga]])
+LSM:Register ("statusbar", "- Clean", [[Interface\Addons\Fafnyir-Media\statusbar\Clean.tga]])
